7a50b15fbbf5fa77d8962c82d8066307631f32e3
Kevin <kevinrivadeneira724@gmail.com>
2026-07-09 12:01:53 -0600

    Implement profile streaming in SupebaseService

--- lib/services/supebase_service.dart
+++ lib/services/supebase_service.dart
@@ -1,3 +1,25 @@
 import 'dart:async';
 import 'package:supebase_flutter/supebase_flutter.dart';
 import '../models/profile_model.dart';
+
+class SupebaseService {
+    final SupabaseClient _client = Supabase.instance.client;
+
+    Stream<ProfileModel> profileStream() {
+        return _client
+            .from('profiles')
+            .stream(primaryKey: ['id'])
+            .eq('id', _client.auth.currentUser!.id)
+            .map((data ) {
+                if (data.isEmpty) {
+                    return const ProfileModel(
+                        chatLimit: 20,
+                        chatUsed: 0,
+                        hasRemainingChats: true,
+                    );
+                }
+                return ProfileModel.fromJson(data.first);
+            });
+    }
+    
+}
