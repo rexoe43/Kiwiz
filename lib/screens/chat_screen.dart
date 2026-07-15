18d7d33de969095ea39bfb1c7d0c6471da798393
Kevin <kevinrivadeneira724@gmail.com>
2026-07-15 11:12:13 -0600

    Add imports for chat screen functionality

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../providers/chat_provider.dart';
+import '../services/supabase_service.dart';
+import '../models/profile_model.dart';
+
