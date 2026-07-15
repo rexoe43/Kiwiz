6d06399ca132678a0b5befa77677a9403971c6de
Kevin <kevinrivadeneira724@gmail.com>
2026-07-15 12:06:31 -0600

    Clean up pubspec.yaml by removing unnecessary comments and retaining essential dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -3,3 +3,11 @@ import 'package:provider/provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'providers/chat_provider.dart';
 import 'screens/chat_screen.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureIntialized();
+
+  await Supbase.initialize(
+    url: 'https://'
+  )
+}
